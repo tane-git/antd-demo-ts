44390ca67c3e0ca1bf9340e7c4c1b6273d17a5ec
tane-git <tane.alexander.wilson@gmail.com>
2022-02-24 16:33:43 +1300

    add grants image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
 import './App.css';
+import { Grants } from './components';
 import { Nav } from './components/Nav';
 
 const App: FC = () => (
   <div className="App">
     <Nav />
+    <Grants />
   </div>
 );
 
